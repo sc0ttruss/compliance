626f28502ddf9c30722d82bf890eb4f4f78c9e5f
Scott <sr@chef.io>
2015-11-28 17:03:32 +0000

    initial release

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'compliance'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures compliance'
+long_description 'Installs/Configures compliance'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,15 @@
+execute 'yum_update' do
+  command 'yum -y update '
+  action :run
+end
+# redhat 6.7
+#chef-compliance-0.9.1-1.el6.x86_64.rpm
+# redhat 7.1
+#chef-compliance-0.9.1-1.el7.x86_64.rpm
+bash 'install_compliance' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+  rpm -Uvh /mnt/share/chef-compliance-0.9.1-1.el6.x86_64.rpm && chef-compliance-ctl reconfigure && chef-compliance-ctl user-create chef chef && chef-compliance-ctl user-create admin chef
+  EOH
+end
