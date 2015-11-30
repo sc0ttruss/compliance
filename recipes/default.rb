72221538fa12bb3da555b512d7057e61f54cc6d6
Scott <sr@chef.io>
2015-11-30 19:37:27 +0200

    add key generation and install in aauthorized_keys file, also add a node to be managed

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,8 @@ bash 'install_compliance' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
-  rpm -Uvh /mnt/share/chef-compliance-0.9.1-1.el6.x86_64.rpm && chef-compliance-ctl reconfigure && chef-compliance-ctl user-create chef chef && chef-compliance-ctl user-create admin chef
+  rpm -Uvh /mnt/share/chef/chef-compliance-0.9.4-1.el7.x86_64.rpm && chef-compliance-ctl reconfigure && chef-compliance-ctl user-create chef chef && chef-compliance-ctl user-create admin chef
+
   EOH
 end
+#https://packagecloud.io/chef/stable/packages/el/6/chef-compliance-0.9.4-1.el6.x86_64.rpm/download
